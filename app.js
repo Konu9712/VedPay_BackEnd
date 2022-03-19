749c3b1670f5b9e835883085af6dbabdc1e07003
Konu <51238256+Konu9712@users.noreply.github.com>
2022-03-19 15:42:44 +0530

    Create a MongoDB Database and connect it to server

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 8000;
+//Database Connection
+require("./database/connection");
 
 //Listener
 app.listen(port, () => {
---
+++ database/connection.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const DB =
+  "mongodb+srv://vedpay:vedpay9712@cluster0.ehzuh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connection successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
