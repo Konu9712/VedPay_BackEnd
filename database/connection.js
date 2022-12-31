2d5ed864d7eddf8cc3f57373f6dbbb766db242e9
Konu <51238256+Konu9712@users.noreply.github.com>
2022-12-31 04:44:15 -0500

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Key = require("dotenv").config();
-const DB = process.env.DB_KEY;
+const DB = "mongodb://vedpay:vedpay9712@cluster0-shard-00-00.ehzuh.mongodb.net:27017,cluster0-shard-00-01.ehzuh.mongodb.net:27017,cluster0-shard-00-02.ehzuh.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-iagg5f-shard-0&authSource=admin&retryWrites=true&w=majority";
 mongoose.connect(
   DB,
   {
